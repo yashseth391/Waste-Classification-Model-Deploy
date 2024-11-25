91f1fbdb8802bc66d78939dfb1aa1d6e91b7ea03
Yash Seth <115554024+yashseth391@users.noreply.github.com>
2024-11-26 02:46:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -44,8 +44,4 @@ async def classify_image(file: UploadFile = File(...)):
 def ans():
     return FileResponse("ans.jpg")
 
-if __name__ == "__main__":
-    import uvicorn
-    import os
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port, log_level="debug")
+
