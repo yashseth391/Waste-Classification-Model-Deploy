2774d728db70f884d1709ac5c4dfcc6530877cce
Yash Seth <115554024+yashseth391@users.noreply.github.com>
2024-11-26 02:29:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,51 @@
-from typing import Optional
+from typing import Union
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import FileResponse
+from pydantic import BaseModel
+from PIL import Image
+import io
+import cv2
+from ultralytics import YOLO
 
-from fastapi import FastAPI
+model_path = "best.pt"
+model = YOLO('yolov8n.pt')
+model = YOLO(model_path)
 
 app = FastAPI()
 
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def read_root():
+    return {"Model Run Successfully"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/predict/")
+async def classify_image(file: UploadFile = File(...)):
+    try:
+        content = await file.read()
+        image = Image.open(io.BytesIO(content))
+        image = image.convert("RGB")
+        new_size = (image.width * 2, image.height * 2)
+        image = image.resize(new_size)
+        image.save("image.jpg")
+        results = model(image)
+        for result in results:
+            result.show()
+            result.save(filename="ans.jpg")
+        img = cv2.imread("ans.jpg")
+        return {"message": "Image saved successfully."}
+    except Exception as e:
+        return {"error": str(e)}
+
+@app.get("/ans/")
+def ans():
+    return FileResponse("ans.jpg")
+
+if __name__ == "__main__":
+    import uvicorn
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port, log_level="debug")
